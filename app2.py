e7e381fa1e271d9476b827f751742005a153eabf
madaweehath <105987361+madaweehath@users.noreply.github.com>
2024-04-06 21:53:07 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -15,7 +15,7 @@ class VideoProcessor:
         # Load pre-trained face detection cascade classifier
         self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-        def detect_faces(self, img):
+    def detect_faces(self, img):
         # Detect faces
         faces = self.face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
         return faces
