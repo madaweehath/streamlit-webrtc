90b2ada8c2f92a70b4fee2beca1e4d0532ed0e4a
madaweehath <105987361+madaweehath@users.noreply.github.com>
2024-04-06 22:58:05 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -14,16 +14,19 @@ class VideoProcessor:
         self.threshold2 = 200
         # Load pre-trained face detection cascade classifier
         self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        self.smile_cascade = cv2.CascadeClassifier( cv2.data.haarcascades + 'haarcascade_smile.xml')
         self.faces = ()
+        self.smiles = ()
 
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
 
         # Detect faces
         faces = self.face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        smiles = smile_cascade.detectMultiScale(img, 1.8, 20)
 
         # Draw rectangles around detected faces
-        for (x, y, w, h) in faces:
+        for (x, y, w, h) in smiles:
             cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
