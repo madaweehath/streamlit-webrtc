ef60d5e31e106fbc4a5cce1fae3012da0df146db
madaweehath <105987361+madaweehath@users.noreply.github.com>
2024-04-07 23:18:52 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -37,6 +37,7 @@ class VideoProcessor:
             smiles = self.smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.8, minNeighbors=20)
             for (sx, sy, sw, sh) in smiles:
                 cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 255, 0), 2)
+                
 #                if len(smiles) > 0:
 #                    self.smile_detected = True
 #                    break  # Exit the loop if a smile is detected
@@ -44,13 +45,12 @@ class VideoProcessor:
         
                 # Check if a smile is detected
         #if self.smile_detected:
-        if len(smiles) > 0:
+        #if len(smiles) > 0:
             # Display an image
-            st.image('Screenshot (137).png', caption='Sunrise by the mountains')    
-            st.subheader('msg after detect smile')
+            #st.image('Screenshot (137).png', caption='Sunrise by the mountains')    
+            #st.subheader('msg after detect smile')
             # Close the camera
             #webrtc_streamer.stop()
-
 
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
