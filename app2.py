37b32d8a67badefbebbc75444c69b049a85488ed
madaweehath <105987361+madaweehath@users.noreply.github.com>
2024-04-09 00:04:31 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -26,15 +26,13 @@ class VideoProcessor:
         
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            if len(faces) > 0:
-                ctx.stop()
-                break
+
             # Region of interest for smile detection within the face
             #roi_gray = img[y:y+h, x:x+w]
             #smiles = self.smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.8, minNeighbors=20)
             #for (sx, sy, sw, sh) in smiles:
              #   cv2.rectangle(roi_gray, (x+sx, y+sy), (x+sx+sw, y+sy+sh), (0, 255, 0), 2)
-        st.write("yay")
+
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 ctx = webrtc_streamer(
@@ -44,3 +42,6 @@ ctx = webrtc_streamer(
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
     }
 )
+    if len(faces) > 0:
+        ctx.stop()
+        st.write("yay")       
