8c816ee653a1c7ecee9a46a6977feff13e02aedb
madaweehath <105987361+madaweehath@users.noreply.github.com>
2024-04-08 23:31:49 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -9,31 +9,32 @@ st.write("Hello, world")
 
 
 
+class VideoProcessor:
+    def __init__(self) -> None:
+        self.threshold1 = 100
+        self.threshold2 = 200
+        # Load pre-trained face detection cascade classifier
+        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        self.smile_cascade = cv2.CascadeClassifier( cv2.data.haarcascades + 'haarcascade_smile.xml')
+        self.faces= ()
+    st.write("faces len:" , len(self.faces))
 
-def __init__(self) -> None:
-    self.threshold1 = 100
-    self.threshold2 = 200
-    # Load pre-trained face detection cascade classifier
-    self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    self.smile_cascade = cv2.CascadeClassifier( cv2.data.haarcascades + 'haarcascade_smile.xml')
-    self.faces= ()
 
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
 
-def recv(self, frame):
-    img = frame.to_ndarray(format="bgr24")
-
-     # Detect faces
-    faces = self.face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    st.write("faces len:" , len(faces))
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        # Region of interest for smile detection within the face
-        roi_gray = img[y:y+h, x:x+w]
-        smiles = self.smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.8, minNeighbors=20)
-        for (sx, sy, sw, sh) in smiles:
-            cv2.rectangle(roi_gray, (x+sx, y+sy), (x+sx+sw, y+sy+sh), (0, 255, 0), 2)
+        # Detect faces
+        faces = self.face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
         
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            # Region of interest for smile detection within the face
+            roi_gray = img[y:y+h, x:x+w]
+            smiles = self.smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.8, minNeighbors=20)
+            for (sx, sy, sw, sh) in smiles:
+                cv2.rectangle(roi_gray, (x+sx, y+sy), (x+sx+sw, y+sy+sh), (0, 255, 0), 2)
+        
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 ctx = webrtc_streamer(
     key="example",
@@ -42,4 +43,3 @@ ctx = webrtc_streamer(
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
     }
 )
-
