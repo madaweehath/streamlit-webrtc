fe06f2d807cda21d5c24bf80d66e9a0955c27897
madaweehath <105987361+madaweehath@users.noreply.github.com>
2024-04-13 18:45:28 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -40,7 +40,7 @@ class VideoProcessor:
         thickness = 2
            
         for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            #cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
             cv2.putText(img, 'hi ' + title , (x, y), font,  fontScale, color, thickness, cv2.LINE_AA)
             flag= True            
             # Region of interest for smile detection within the face
@@ -48,7 +48,9 @@ class VideoProcessor:
             smiles = self.smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.1, minNeighbors=30, minSize=(20, 20))
 
             for (sx, sy, sw, sh) in smiles:
-                cv2.rectangle(roi_gray, (sx, sy), (sx+sw, sy+sh), (0, 255, 0), 2)
+                #cv2.rectangle(roi_gray, (sx, sy), (sx+sw, sy+sh), (0, 255, 0), 2)
+                cv2.putText(img, 'عيدك مبارك يـ ' + title , (x, y), font,  fontScale, color, thickness, cv2.LINE_AA)
+
             
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
