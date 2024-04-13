063f9f7b01548ed11caed5cfbe7be4da5577c79e
madaweehath <105987361+madaweehath@users.noreply.github.com>
2024-04-13 19:18:25 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -49,7 +49,8 @@ class VideoProcessor:
 
             for (sx, sy, sw, sh) in smiles:
                 #cv2.rectangle(roi_gray, (sx, sy), (sx+sw, sy+sh), (0, 255, 0), 2)
-                cv2.rectangle(roi_gray, (x+sx, y+sy), (sx+sw, sy+sh), (0, 255, 0), 2)
+                #cv2.rectangle(roi_gray, (x+sx, y+sy), (sx+sw, sy+sh), (0, 255, 0), 2)
+                cv2.rectangle(roi_gray, (x+sx, y+sy), (x+sx+sw, y+sy+sh), (0, 255, 0), 2)
                 cv2.putText(img, 'Eid Mubarak ' + title , (x, y), font,  fontScale, color, thickness, cv2.LINE_AA)
 
             
